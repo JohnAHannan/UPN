e010451161ab599b453c627606ca14d6b15efc59
Jafh <hannjohn12@gmail.com>
2022-01-31 14:13:45 -0600

    First Commit Working Main menu

--- src/main/java/com/example/upn/MenuApplication.java
+++ src/main/java/com/example/upn/MenuApplication.java
@@ -24,8 +24,6 @@ public class MenuApplication extends Application {
         stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
-
-
     }
 
     public static void main (String[] args) {
